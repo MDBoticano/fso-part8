c791b2046e2e5fbb1612623ffc318d7f9391fbf0
mdboticano <mdboticano@gmail.com>
2019-09-26 14:54:30 -0700

    8.7: Implemented mutation to edit author birth year

--- library/library-backend.js
+++ library/library-backend.js
@@ -110,6 +110,10 @@ const typeDefs = gql`
       published: Int,
       genres: [String!]!
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int
+    ): Author
   }
 `
 
@@ -162,6 +166,15 @@ const resolvers = {
       books = [...books, newBook]
 
       return newBook
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(auth => auth.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = {...author, born: args.setBornTo}
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
